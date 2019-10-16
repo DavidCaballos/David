cc88c54d4a7fcb12d5ff1e90cfa5dd3bbcd5802b
Francisco Gordillo <javier.gordillo.diaz2000@gmail.com>
2019-10-16 11:13:17 +0200

    aula update

--- update.sql
+++ update.sql
@@ -2,7 +2,10 @@ UPDATE Usuario
 SET nombre_alumno="Juan";
 
 UPDATE curso
-SET codigo_curso="14";
+SET codigo_curso=14;
 
 update edificio
-set codigo_edificio="7"
+set codigo_edificio=7;
+
+update aula
+set capacidad=20;
